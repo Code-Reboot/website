c97545fe35046ac7a9c0bac728af9478a889fbd8
TheNewJavaman <gpizarro@javaman.net>
2017-07-19 08:07:16 -0700

    ISSUES STILL EXIST, Signup not finished

--- javascript/signup.js
+++ javascript/signup.js
@@ -1,4 +1,4 @@
-function submit() {
+function submitEmail() {
   var button = document.getElementById('submit');
   button.onclick = function() {
     var parent = document.getElementById('parent').value;
@@ -9,10 +9,10 @@ function submit() {
     var comments = document.getElementById('comments').value;
 
     var mailto = 'mailto:gpizarro@javaman.net';
-    var subject = 'subject=I Would Like To Sign My Child Up';
+    var subject = '&subject=I%20Would%20Like%20To%20Sign%20My%20Child%20Up';
     var body = '&body=';
     body += 'I would like to sign ' + student + ' up for Code Reboot 2017.';
-    body += ' ' + student + ' is ' + ' years old.';
+    body += ' ' + student + ' is ' + age + ' years old.';
     if(allergies != '') {
       body += '%0A %0AAllergies: %0A' + allergies;
     }
